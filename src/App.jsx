56ce3fdcb7836755d4a9bfee6007f5c991e526fd
virginiabarrios <virginia.aye.bar@gmail.com>
2024-04-07 22:19:02 -0300

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Hero from './Hero'
+import Destacados from './Destacados'
+
+function App() {
+  return (
+      <>
+        <Hero />
+        <Destacados />
+      
+      </>
+  )
+}
+
+export default App
